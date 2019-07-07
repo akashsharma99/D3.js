5573c035e0cd03ec26203168a44212fc24eb105a
akashsharma99 <akash.asharma.sharma11@gmail.com>
2019-07-07 11:41:08 +0530

    bar chart complete

--- course practice files/4 linearScale/index.js
+++ course practice files/4 linearScale/index.js
@@ -1,31 +1,59 @@
 //select svg container first
-const svg=d3.select('svg');
+const svg=d3.select('.canvas')
+.append('svg')
+.attr('width',600)
+.attr('height',600);
+//create margins and dimensions
+const margin={top:20,right:20,bottom:100,left:100};
+const graphWidth=600-margin.left-margin.right;
+const graphHeight=600-margin.top-margin.bottom;
+const graph=svg.append('g')
+.attr('width',graphWidth)
+.attr('height',graphHeight)
+.attr('transform',`translate(${margin.left},${margin.top})`);
+
+const xAxisGroup=graph.append('g')
+.attr('transform',`translate(0,${graphHeight})`);
+const yAxisGroup=graph.append('g');
+
 d3.json('menu.json').then(data=>{
+
+     // const min=d3.min(data,d=>d.orders);
+     // const max=d3.max(data,d=>d.orders);
+     // const extent=d3.extent(data,d=>d.orders);
+     // console.log(min,max,extent);
+
      const y=d3.scaleLinear()
-     .domain([0,1000])
-     .range([0,500]);
+     .domain([0,d3.max(data,d=>d.orders)])
+     .range([graphHeight,0]);
+
+
      const x=d3.scaleBand()
      .domain(data.map(item=>item.name))
      .range([0,500])
      .paddingInner(0.2)
      .paddingOuter(0.2);
-     console.log(x("chowmein"));
-     console.log(y(400));
-     console.log(y(1000));
-     console.log(y(200));
-     console.log(y(450));
+     
      //join the data to rectangles
-     const rects=svg.selectAll('rect').data(data);
+     const rects=graph.selectAll('rect').data(data);
 
      rects.attr('width',x.bandwidth)
-     .attr('height',d=>y(d.orders))
+     .attr('height',d=>graphHeight-y(d.orders))
      .attr('fill','orange')
-     .attr('x',d=>x(d.name));
+     .attr('x',d=>x(d.name))
+     .attr('y',d=>y(d.orders));
 
      rects.enter()
      .append('rect')
      .attr('width',x.bandwidth)
-     .attr('height',d=>y(d.orders))
+     .attr('height',d=>graphHeight-y(d.orders))
      .attr('fill','orange')
-     .attr('x',d=>x(d.name));
+     .attr('x',d=>x(d.name))
+     .attr('y',d=>y(d.orders));
+     //create and call the axes
+     const xAxis=d3.axisBottom(x);
+     const yAxis=d3.axisLeft(y);
+
+     xAxisGroup.call(xAxis);
+     yAxisGroup.call(yAxis);
 })
